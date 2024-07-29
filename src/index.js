a560a7f21c2ab92dde308d45afa534c60734d5dd
dat27022002 <dat.nguyen27022002@hcmut.edu.vn>
2024-07-29 15:34:27 +0700

    conf: install and config redux

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { I18nextProvider } from 'react-i18next';
+import { Provider } from 'react-redux';
+
+import GlobalStyles from '~/components/GlobalStyles';
 import App from '~/App';
 import reportWebVitals from './reportWebVitals';
-import GlobalStyles from '~/components/GlobalStyles';
-
-import { I18nextProvider } from 'react-i18next';
 import i18n from './utils/i18n';
+import store from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <I18nextProvider i18n={i18n}>
             <GlobalStyles>
-                <App />
+                <Provider store={store}>
+                    <App />
+                </Provider>
             </GlobalStyles>
         </I18nextProvider>
     </React.StrictMode>,
