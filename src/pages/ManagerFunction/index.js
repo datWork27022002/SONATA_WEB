bc2b597db3d6c04e6fb90d3b277e14a436dd4caa
dat27022002 <dat.nguyen27022002@hcmut.edu.vn>
2024-08-27 10:01:45 +0700

    feat: Manager Function/ Time Card screen UI

--- src/pages/ManagerFunction/index.js
+++ src/pages/ManagerFunction/index.js
@@ -1,5 +1,6 @@
 import Announcement from './Announcement';
 import Payroll from './Payroll';
+import TimeCard from './TimeCard';
 
-export { Announcement, Payroll };
+export { Announcement, Payroll, TimeCard };
 export { default } from './ManagerFunction';
--- src/routes/index.js
+++ src/routes/index.js
@@ -12,7 +12,7 @@ import { ChangeOfMultilingualName, ItemLinkedCodeManagement, MainMenuSetting } f
 import { TableLocationManager, TableDesignSetting, POSFavoriteFunctionButton } from '~/pages/BasicSettings';
 import { PaymentButtonSetting, KioskImageSettings, UPOrderImageSettings } from '~/pages/BasicSettings';
 import { KioskFunctionSetting, POSUserDefinitionLanguage } from '~/pages/BasicSettings';
-import ManagerFunction, { Announcement, Payroll } from '~/pages/ManagerFunction';
+import ManagerFunction, { Announcement, Payroll, TimeCard } from '~/pages/ManagerFunction';
 import Customer from '~/pages/Customer';
 import OperationReports from '~/pages/OperationReports';
 import SaleReports from '~/pages/SaleReports';
@@ -73,6 +73,7 @@ const basicSettingsRoute = [
 const managerFunctionRoute = [
     { path: pathManagerFunction.ANNOUNCEMENT, component: Announcement },
     { path: pathManagerFunction.PAYROLL, component: Payroll },
+    { path: pathManagerFunction.TIME_CARD, component: TimeCard },
 ];
 
 export const publicRoute = [
